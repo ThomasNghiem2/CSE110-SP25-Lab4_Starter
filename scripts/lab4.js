5cd6630823deec262c5b68e3851335c2e8d1bf9d
Thomas Nghiem <tnghiem@ucsd.edu>
2025-04-30 00:20:41 -0700

    fixed type checking errors

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if ((typeof num1 != Number) || (typeof num2 != Number)) {
+    if ((typeof num1 != "number") || (typeof num2 != "number")) {
         return false;
     }
 
@@ -29,7 +29,7 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if ((typeof prices != Array) || (typeof discount != Number)) {
+    if ((Array.isArray(prices) == false) || (typeof discount != "number")) {
         return false;
     }
     const discounted = []
